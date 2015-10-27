bd2e25b488c075a7c235391c6e8b0e17a94e7e83
Sarah Hersh <schersh@gmail.com>
2015-10-26 20:56:12 -0400

    fixed checkClass function

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // game state variables
-var newPics = [
-  '1', '1',
-  '2', '2',
-  '3', '3',
-  '4', '4',
-  '5', '5',
-  '6', '6'
-];
+// var newPics = [
+//   '1', '1',
+//   '2', '2',
+//   '3', '3',
+//   '4', '4',
+//   '5', '5',
+//   '6', '6'
+// ];
 
-var grid = [
-  'box1', 'box2', 'box3',
-  'box4', 'box5', 'box6',
-  'box7', 'box8', 'box9',
-  'box10', 'box11', 'box12'
-];
-// functions
+// var grid = [
+//   'box1', 'box2', 'box3',
+//   'box4', 'box5', 'box6',
+//   'box7', 'box8', 'box9',
+//   'box10', 'box11', 'box12'
+// ];
 
-// var keepScore =
+// // functions
 
 // var shufflePics = function (newPics) {
 //   var m = newPics.length, t, i;
@@ -55,20 +54,26 @@ var grid = [
 //     boxId.css('background', 'orange');
 //   }
 // }
-// Need a function that will watch all boxids and remove on.click event when two boxids have "showPic" class. When they see 2, call cardMatch (card 1, card 2)
-// var checkClass = function () {
-//   $('.box').each
-//   for (var i = 0; i < grid.length; i++) {
-//     var clicked = [
-//       ($('#[i]').attr('showPic')
-//     ]
-//   };
-//   if (clicked.length == 2) {
-//     $('.box').off('click', handleBoxClick)
-//   };
-// };
-//   or ($('#[i]').attr('class') === 'showPic'
-//
+
+// Need a function that will watch all boxes and remove on.click event when two boxids have "showPic" class. When they see 2, call cardMatch (card 1, card 2)
+var checkClass = function () {
+  var showPicBoxes = [];
+  for (var i = 0; i < $('.box').length; i++) {
+    var targetID = '#box' + i;
+    var boxClass = $(targetID).hasClass('showPic');
+    if (boxClass) {
+      showPicBoxes.push(targetID);
+    }
+  };
+  if (showPicBoxes.length == 2) {
+    return(showPicBoxes);
+  }
+  else {
+    return false;
+  }
+
+};
+
 
 
 // var cardMatch = function(boxId1, boxId2) {
@@ -78,18 +83,30 @@ var grid = [
 //   }
 // }
 
+// var keepScore = function() {
+//   if (true) {
+//     matched+1
+//   }
+//   else {
+//     missed+1
+//   }
+// }
+
 // event handlers
 var handleBoxClick = function(evt) {
   var clickedBox = this;
   var boxId = $(clickedBox).attr('id');
-  $(clickedBox).toggleClass('showPic')
+  $(clickedBox).toggleClass('showPic');
+  $(clickedBox).removeClass('noPic')
+  checkClass();
   //or  $(this).append('.showPic')
 }
-
 var handleDealButton = function(evt) {
   $('.box').toggleClass('noPic');
-  shufflePics();
+  $('.box').removeClass('showPic');
+  // shufflePics();
 }
+
 // event listeners
 $('.box').on('click', handleBoxClick);
 $('#deal').on('click', handleDealButton);
